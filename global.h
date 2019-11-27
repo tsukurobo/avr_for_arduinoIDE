fbd8a0150cea41263919e2e05517a831431e1c9a
0x005c <52650857+0x005c@users.noreply.github.com>
2019-11-27 15:21:59 +0900

    Add dummy files
    Add dummy dip.h dip.c
    Add global.h
    Add main.c

---
+++ global.h
@@ -0,0 +1,22 @@
+#ifndef IMD_GLOBAL_H
+#define IMD_GLOBAL_H
+
+#include <Arduino.h>
+
+typedef enum I2CAddrMode {
+  I2C_PRESET,
+  I2C_ADDR1,
+  I2C_ADDR2,
+} I2CAddrMode;
+
+typedef enum PIDMode {
+  PID_POSITION,
+  PID_VELOCITY,
+} PIDMode;
+
+bool is_pid, is_brake, is_test;
+I2CAddrMode i2c_addr_mode;
+PIDMode pid_mode;
+
+#endif
+
